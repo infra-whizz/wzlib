3d27515543dcaf09dbad6698cab54ddf2b97f17c
Bo Maryniuk <bo@maryniuk.net>
2020-03-18 23:12:46 +0100

    Add response channel

--- const.go
+++ const.go
@@ -1,8 +1,9 @@
-package wzlib_const
+package wzlib
 
 const (
 	CHANNEL_PUBLIC     = "public"     // Public channel for all clients
 	CHANNEL_PRIVATE    = "private"    // Private channel: payload is encrypted
 	CHANNEL_CONSOLE    = "remote"     // Remote controller is talking here and controller is listening to it
 	CHANNEL_CONTROLLER = "controller" // Controller is listening here
+	CHANNEL_RESPONSE   = "response"   // Response from the clients
 )
