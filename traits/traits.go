5ebd9bc6ee9ddebaaad3acaa2e22d7b7e3ee8064
Bo Maryniuk <bo@maryniuk.net>
2020-03-23 14:01:48 +0100

    Add traits loader

---
+++ traits/traits.go
@@ -0,0 +1,34 @@
+package wzlib_traits
+
+// WzTraits struct
+type WzTraits struct {
+	container      *WzTraitsContainer
+	traitsFileName string
+}
+
+// NewTraits constructor.
+/*
+  WzTraits is a compount object that is loading all possible traits,
+  registered to it and returns a self-contained instance.
+*/
+func NewTraits(fpath string) *WzTraits {
+	t := new(WzTraits)
+	t.traitsFileName = fpath
+	t.container = NewWzTraitsContainer().LoadFromFile(t.traitsFileName)
+	return t
+}
+
+// LoadAttribute to the WzTraits container
+func (tl *WzTraits) LoadAttribute(attr TraitsAttribute) {
+	attr.Load(tl.container)
+}
+
+// Save traits data to a file
+func (tl *WzTraits) Save() {
+	tl.container.SaveToFile(tl.traitsFileName)
+}
+
+// Load traits data from a file
+func (tl *WzTraits) Load() {
+	tl.container.LoadFromFile(tl.traitsFileName)
+}
