dd5957880ee3dee558fb485994a07380a5476bef
Bo Maryniuk <bo@maryniuk.net>
2020-03-23 21:12:54 +0100

    Add network information trait

---
+++ traits/attributes/netinfo.go
@@ -0,0 +1,61 @@
+package wzlib_traits_attributes
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/elastic/go-sysinfo"
+
+	wzlib_traits "github.com/infra-whizz/wzlib/traits"
+	"github.com/shirou/gopsutil/net"
+)
+
+// NetInfo class
+type NetInfo struct {
+	KEY_PREFIX string
+	container  *wzlib_traits.WzTraitsContainer
+}
+
+// NewNetInfo is a constructor to SysInfo class
+func NewNetInfo() *NetInfo {
+	ni := new(NetInfo)
+	ni.KEY_PREFIX = "net"
+	return ni
+}
+
+// Load all sub-attributes
+func (ni *NetInfo) Load(container *wzlib_traits.WzTraitsContainer) {
+	ni.container = container
+	ni.interfaces()
+	ni.network()
+}
+
+func (ni *NetInfo) network() {
+	host, err := sysinfo.Host()
+	if err != nil {
+		log.Println("Error getting sysinfo for the host:", err.Error())
+	}
+	nfo := host.Info()
+	ni.container.Set(fmt.Sprintf("%s.MAC", ni.KEY_PREFIX), nfo.MACs)
+	ni.container.Set(fmt.Sprintf("%s.IP", ni.KEY_PREFIX), nfo.IPs)
+}
+
+func (ni *NetInfo) interfaces() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Println("Error getting network interface:", err.Error())
+		return
+	}
+
+	for _, iface := range ifaces {
+		addresses := make([]string, 0)
+		for _, addr := range iface.Addrs {
+			addresses = append(addresses, addr.Addr)
+		}
+		if len(addresses) > 0 && iface.HardwareAddr != "" {
+			ni.container.Set(fmt.Sprintf("%s.%s.addr", ni.KEY_PREFIX, iface.Name), addresses)
+			ni.container.Set(fmt.Sprintf("%s.%s.hw_addr", ni.KEY_PREFIX, iface.Name), iface.HardwareAddr)
+			ni.container.Set(fmt.Sprintf("%s.%s.flags", ni.KEY_PREFIX, iface.Name), iface.Flags)
+		}
+	}
+}
