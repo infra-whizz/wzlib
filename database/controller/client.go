9303aba63a3d70f4a8bdc3500570e07e24c4fa0d
Bo Maryniuk <bo@maryniuk.net>
2020-03-31 22:04:44 +0200

    Declare table fields unique

--- database/controller/client.go
+++ database/controller/client.go
@@ -1,9 +1,9 @@
 package wzlib_database_controller
 
 type WzClient struct {
-	ID     int `gorm:"primary_key"`
-	Uid    string
-	Fqdn   string
-	RsaPk  string
-	Status int
+	ID     int    `gorm:"primary_key"`
+	Uid    string `gorm:"unique; not null"`
+	Fqdn   string `gorm:"unique; not null"`
+	RsaPk  string `gorm:"unique; not null"`
+	Status int    `gorm:"not null"`
 }
