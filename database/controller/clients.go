c24442b4636c33397b4f6652e9b5bffe05bc6880
Bo Maryniuk <bo@maryniuk.net>
2020-03-31 17:08:19 +0200

    Add clients API ideas

---
+++ database/controller/clients.go
@@ -0,0 +1,69 @@
+package wzlib_database_controller
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+// Everything with client
+type WzCtrlClientsAPI struct {
+	db *gorm.DB
+}
+
+func NewWzCtrlClientsAPI() *WzCtrlClientsAPI {
+	wcca := new(WzCtrlClientsAPI)
+	return wcca
+}
+
+func (wcc *WzCtrlClientsAPI) setDbh(dbh *gorm.DB) *WzCtrlClientsAPI {
+	wcc.db = dbh
+	return wcc
+}
+
+// Register register a client that just appeared.
+// Registration means "Your public RSA is in the database, now wait"
+func (wcc *WzCtrlClientsAPI) Register(client *WzClient) {
+
+}
+
+// Accept that was already registered.
+// Accepetation means flipping status and it will be "OK, now you are in".
+// This makes client listable for the workers.
+// But the reconciliation needs to be extra called elsewhere.
+func (wcc *WzCtrlClientsAPI) Accept() {}
+
+// Reject sets its status as "rejected", but keeps in the database
+// everything: FQDN, machine ID and RSA pubkey. Used for black-listing.
+func (wcc *WzCtrlClientsAPI) Reject() {}
+
+// Delete just deletes everything of the client.
+// This client is eligible to be registered again.
+func (wcc *WzCtrlClientsAPI) Delete() {}
+
+// GetRegistered returns a list of new clients
+func (wcc *WzCtrlClientsAPI) GetNew() {}
+
+// GetRejected returns a list of new clients
+func (wcc *WzCtrlClientsAPI) GetRejected() {}
+
+// GetRegisteredAmount returns an amout of registered clients
+func (wcc *WzCtrlClientsAPI) GetRegisteredAmount() int64 {
+	return 0
+}
+
+// Search for clients based on specific query
+func (wcc *WzCtrlClientsAPI) Search() {}
+
+// GetByFQDN returns client data (struct?) by FQDN
+func (wcc *WzCtrlClientsAPI) GetByFQDN() {}
+
+// GetByUid returns client data (struct?) by system ID
+func (wcc *WzCtrlClientsAPI) GetByUid() {}
+
+// GetByFp returns client data (struct?) by RSA fingerprint
+func (wcc *WzCtrlClientsAPI) GetByFp() {}
+
+// Set sets/updates/adds client's status (struct?). This does not include traits.
+func (wcc *WzCtrlClientsAPI) Set() {}
+
+// UpdateTraits adds/sets/updates client's traits
+func (wcc *WzCtrlClientsAPI) UpdateClientTraits() {}
