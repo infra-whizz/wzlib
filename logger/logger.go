54e19f24dda39eed426577ba647ff7105be48b04
Bo Maryniuk <bo@maryniuk.net>
2020-04-02 16:17:53 +0200

    keep the same logger instance for everything

--- logger/logger.go
+++ logger/logger.go
@@ -1,31 +1,39 @@
 package wzlib_logger
 
 import (
+	"io/ioutil"
 	"os"
 
 	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/sirupsen/logrus"
 )
 
-type WzLogger struct {
-	log   *logrus.Logger
-	level logrus.Level
-}
+var _Logger *logrus.Logger
+
+type WzLogger struct{}
 
 // SetLogLevel sets a log level
 func (wl *WzLogger) SetLogLevel(level logrus.Level) {
-	wl.level = level
-	if wl.log != nil {
-		wl.log.SetLevel(wl.level)
-	}
+	wl.GetLogger().SetLevel(level)
+}
+
+// MuteLoggerToPanic logs only panic messages, when program is crashing
+func (wl *WzLogger) MuteLoggerToPanic() {
+	wl.SetLogLevel(logrus.PanicLevel)
+}
+
+// MuteLogger completely mutes the logger, discarding everything
+func (wl *WzLogger) MuteLogger() {
+	wl.MuteLoggerToPanic()
+	wl.GetLogger().SetOutput(ioutil.Discard)
 }
 
 // GetLogger returns lazy-instantiated logger.
 func (wl *WzLogger) GetLogger() *logrus.Logger {
-	if wl.log == nil {
-		wl.log = GetTextLogger(logrus.TraceLevel, nil)
+	if _Logger == nil {
+		_Logger = GetTextLogger(logrus.TraceLevel, nil)
 	}
-	return wl.log
+	return _Logger
 }
 
 // GetTextLogger create a logger instance
@@ -36,7 +44,6 @@ func GetTextLogger(level logrus.Level, out *os.File) *logrus.Logger {
 	formatter.ShowFullLevel = true
 
 	logger := logrus.New()
-
 	if out == nil {
 		logger.Out = os.Stderr
 		formatter.NoColors = false
@@ -49,7 +56,6 @@ func GetTextLogger(level logrus.Level, out *os.File) *logrus.Logger {
 
 	logger.Level = level
 	logger.SetFormatter(formatter)
-
 	return logger
 }
 
