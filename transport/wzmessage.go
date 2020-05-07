ed043488b374a7cc244a37a8bc448c571eea75e9
Bo Maryniuk <bo@maryniuk.net>
2020-05-07 21:59:34 +0200

    Add signature payload slot

--- transport/wzmessage.go
+++ transport/wzmessage.go
@@ -15,10 +15,11 @@ const (
 
 // Message keys
 const (
-	PAYLOAD_RSA         = "rsa.pub"     // cipher text in PEM
-	PAYLOAD_SYSTEM_ID   = "system.id"   // unique string of system ID
-	PAYLOAD_SYSTEM_FQDN = "system.fqdn" // Host FQDN or just a hostname
-	PAYLOAD_PING_ID     = "ping.id"     // ID of a ping request
+	PAYLOAD_RSA           = "rsa.pub"       // cipher text in PEM
+	PAYLOAD_RSA_SIGNATURE = "rsa.signature" // signature
+	PAYLOAD_SYSTEM_ID     = "system.id"     // unique string of system ID
+	PAYLOAD_SYSTEM_FQDN   = "system.fqdn"   // Host FQDN or just a hostname
+	PAYLOAD_PING_ID       = "ping.id"       // ID of a ping request
 
 	/*
 		Function return payload. The value is a nested string/interface mapping
