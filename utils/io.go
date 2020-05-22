001230e3d78ea4c751afe859d394893faa4a243e
Bo Maryniuk <bo@maryniuk.net>
2020-05-23 00:16:40 +0200

    Add utility to check if file exists

---
+++ utils/io.go
@@ -0,0 +1,12 @@
+package wzlib_utils
+
+import "os"
+
+// FileExists checks if file... well, exists.
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
