a195eab023ca6467c4b445e1fe7f10b7b2bf0846
Bo Maryniuk <bo@maryniuk.net>
2020-07-09 19:55:29 +0200

    Add content detection for the local files

--- utils/io.go
+++ utils/io.go
@@ -1,6 +1,10 @@
 package wzlib_utils
 
-import "os"
+import (
+	"net/http"
+	"os"
+	"strings"
+)
 
 // FileExists checks if file... well, exists.
 func FileExists(filename string) bool {
@@ -9,4 +13,27 @@ func FileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+// FileContentType detects the content type via http.
+// If unable to detect a proper one, "application/octet-stream" is returned.
+func FileContentType(out *os.File) (string, error) {
+	buff := make([]byte, 0x200)
+	_, err := out.Read(buff)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Split(http.DetectContentType(buff), ";")[0], nil
+}
+
+// FileContentTypeByPath is a convenience alias to FileContentType to handle paths in strings.
+func FileContentTypeByPath(pth string) (string, error) {
+	fp, err := os.Open(pth)
+	if err != nil {
+		return "", err
+	}
+	defer fp.Close()
+
+	return FileContentType(fp)
 }
