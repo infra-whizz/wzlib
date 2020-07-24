c44d4a6b9429b47e6292c0ef198508f26bbf5990
Bo Maryniuk <bo@maryniuk.net>
2020-07-24 13:46:19 +0200

    Use the same logger instance

--- subprocess/subprocess_nix.go
+++ subprocess/subprocess_nix.go
@@ -1,17 +1,15 @@
 package wzlib_subprocess
 
 import (
-	"os"
 	"os/exec"
 	"strings"
 
 	wzlib_logger "github.com/infra-whizz/wzlib/logger"
-	"github.com/sirupsen/logrus"
 )
 
 // ExecCommand is a small platform specific wrapper around os/exec.Command
 func ExecCommand(name string, arg ...string) *Cmd {
-	wzlib_logger.GetTextLogger(logrus.DebugLevel, os.Stderr).Debugf("Call: %s %s", name, strings.Join(arg, " "))
+	wzlib_logger.GetCurrentLogger().Debugf("Call: %s %s", name, strings.Join(arg, " "))
 	cmd := exec.Command(name, arg...)
 	return newCmd(cmd)
 }
